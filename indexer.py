9e96a91970773c1ce73ac6d76b88db66ea849cf3
Crystal Chiu <chiubacca@Crystals-Air.lan>
2024-11-15 01:58:46 -0800

    adding multi way merging

--- indexer.py
+++ indexer.py
@@ -1,11 +1,13 @@
 import os
 import json
 from collections import defaultdict
+from heapq import merge
+
 from tokenizer import tokenize
 
 # CONST GLOBALS
 # needs experimenting
-_CHUNK_SIZE = 4000
+_CHUNK_SIZE = 10000
 
 class Indexer:
     # Create the index dictionary DS
@@ -20,6 +22,7 @@ class Indexer:
         self.index_kbs = 0
         self.unique_tokens = set()
 
+    # Extracts the tokens and their freq from the current document to make posting and put each token into index
     def add_document(self, doc_id, content_tokens):
         # freq dict of each token in given doc
         term_frequency = defaultdict(int)
@@ -36,6 +39,7 @@ class Indexer:
             }
             self.inverted_index[token].append(posting)
 
+    # Dumps contents of current index into a new file and clears it
     def save_partial_index(self, partial_index_num):
         partial_index_path = os.path.join(self.partial_index_dir, f"partial_index_{partial_index_num}.json")
 
@@ -45,43 +49,97 @@ class Indexer:
         self.inverted_index.clear()
 
     def build_index(self, documents):
-        global _CHUNK_SIZE
-
-        # create dir to store partial indexes if not exist
         os.makedirs(self.partial_index_dir, exist_ok=True)
         partial_index_num = 0
+        documents_iter = iter(documents.items())
 
-        for doc_id, content in documents.items():
-            content_tokens = tokenize(content)
-            self.add_document(doc_id, content_tokens)
-            self.doc_count += 1
+        while True:
+            try:
+                for _ in range(_CHUNK_SIZE):
+                    doc_id, content = next(documents_iter)
+                    content_tokens = tokenize(content)
+                    self.add_document(doc_id, content_tokens)
+                    self.doc_count += 1
 
-            # we now need dump current partial index and start another
-            if self.doc_count % _CHUNK_SIZE == 0:
                 self.save_partial_index(partial_index_num)
                 partial_index_num += 1
+            except StopIteration:
+                break
 
-        # make sure we save the last partial index built if has content
         if self.inverted_index:
             self.save_partial_index(partial_index_num)
 
-    def merge_indexes(self):
-        # TODO
+    # def build_index(self, documents):
+    #     global _CHUNK_SIZE
+    #
+    #     # create dir to store partial indexes if not exist
+    #     os.makedirs(self.partial_index_dir, exist_ok=True)
+    #     partial_index_num = 0
+    #
+    #     # NOTE: We should store the partial indexes in alphabetic order
+    #
+    #     # TODO: we need to make a batch of documents
+    #     # TODO: modify to have a while loop (While Documents not empty)
+    #     # TODO: Get batch from documents, (for all docs in B...)
+    #     for doc_id, content in documents.items():
+    #         content_tokens = tokenize(content)
+    #         self.add_document(doc_id, content_tokens)
+    #         self.doc_count += 1
+    #
+    #         # we now need dump current partial index and start another
+    #         if self.doc_count % _CHUNK_SIZE == 0:
+    #             self.save_partial_index(partial_index_num)
+    #             partial_index_num += 1
+    #
+    #     # TODO: now we need to SORT and write to disk!
+    #
+    #     # make sure we save the last partial index built if has content
+    #     if self.inverted_index:
+    #         self.save_partial_index(partial_index_num)
 
-        for partial_index_num in range(len(os.listdir(self.partial_index_dir))):
-            partial_index_path = os.path.join(self.partial_index_dir, f"partial_index_{partial_index_num}.json")
+    def multi_way_merge(self):
+        partial_files = [
+            os.path.join(self.partial_index_dir, f)
+            for f in os.listdir(self.partial_index_dir) if f.startswith("partial_index_")
+        ]
 
-            if os.path.exists(partial_index_path):
-                with open(partial_index_path, 'r', encoding='utf-8') as file:
-                    partial_index = json.load(file)
+        # open all partial index files and load iterators for each
+        partial_file_iters = []
+        for file in partial_files:
+            with open(file, 'r', encoding='utf-8') as f:
+                partial_index = json.load(f)
+                partial_file_iters.append(iter(sorted(partial_index.items())))
 
-                    # merge partial index with main index for overlapping terms etc
-                    for token, postings in partial_index.items():
-                        self.inverted_index[token].extend(postings)
+        # merging iterators on the token (key) to avoid loading all data into memory
+        merged_index = defaultdict(list)
+        for token, postings in merge(*partial_file_iters, key=lambda x: x[0]):
+            merged_index[token].extend(postings)
 
-        # write into its own file
+        # save final merged index to file
         with open(self.final_index_file, 'w', encoding='utf-8') as file:
-            json.dump(self.inverted_index, file)
+            json.dump(merged_index, file)
 
-        # set size of final merged index
+        # calculate final index size in KB
         self.index_kbs = os.path.getsize(self.final_index_file) / 1024
+
+    # After index is built, merges all partial indexes into one file
+    # def merge_indexes(self):
+    #     # TODO: WE should modify to do either a binary merge (2 files at a time) or a mult way merge (all files simultaneously)
+    #
+    #     for partial_index_num in range(len(os.listdir(self.partial_index_dir))):
+    #         partial_index_path = os.path.join(self.partial_index_dir, f"partial_index_{partial_index_num}.json")
+    #
+    #         if os.path.exists(partial_index_path):
+    #             with open(partial_index_path, 'r', encoding='utf-8') as file:
+    #                 partial_index = json.load(file)
+    #
+    #                 # merge partial index with main index for overlapping terms etc
+    #                 for token, postings in partial_index.items():
+    #                     self.inverted_index[token].extend(postings)
+    #
+    #     # write into its own file
+    #     with open(self.final_index_file, 'w', encoding='utf-8') as file:
+    #         json.dump(self.inverted_index, file)
+    #
+    #     # set size of final merged index
+    #     self.index_kbs = os.path.getsize(self.final_index_file) / 1024
--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from indexer import Indexer
 def print_summary(number_indexed_docs, number_unique_tokens, total_kbs):
     print(f"Number of Indexed Documents: {number_indexed_docs} \n"
           f"Number of Unique Tokens: {number_unique_tokens} \n"
-          f"Total Number KBs of Index: {total_kbs}")
+          f"Total Number KBs of Index: {total_kbs} kbs")
 
 def main():
     # load documents from data directory
     # data_dir = "DEV"  # path to the json files containing html
-    data_dir = "TEST"
+    data_dir = "DEV"
     documents = load_documents(data_dir)
 
     # initialize indexer and build the index
     partial_index_dir = "PARTIAL_INDEXES"
     indexer = Indexer(partial_index_dir)
     indexer.build_index(documents)
-    indexer.merge_indexes()
+    indexer.multi_way_merge()
 
     # retrieve & print metrics:
     number_indexed_docs = len(documents)
